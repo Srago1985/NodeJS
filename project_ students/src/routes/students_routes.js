0203b65bbb513841c532311c8127812919df3785
Srago1985 <mihanik999@gmail.com>
2026-06-18 21:56:53 +0300

    controller and router updated

--- project_ students/src/routes/students_routes.js
+++ project_ students/src/routes/students_routes.js
@@ -1,9 +1,15 @@
-import router from 'express';
-import { addStudent, findStudent } from '../controller/student_controller.js';
+import { Router } from 'express';
+import { addStudent, findStudent, deleteStudent, updateStudent, addScore, findStudentsByName, countStudentsByNames, findStudentsByMinScore } from '../controller/student_controller.js';
 
-const router = router();
+const router = Router();
 
 router.post('/student', addStudent);
 router.get('/student/:id', findStudent);
+router.delete('/student/:id', deleteStudent);
+router.patch('/student/:id', updateStudent);
+router.patch('/score/student/:id', addScore);
+router.get('/students/name/:name', findStudentsByName);
+router.get('/quantity/students', countStudentsByNames);
+router.get('/students/exam/:exam/min-score/:minScore', findStudentsByMinScore);
 
 export default router;
